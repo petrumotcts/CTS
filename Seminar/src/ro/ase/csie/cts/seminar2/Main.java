0c0bf3a1aaf0efceb6d8b67e78a5120b6fa1022b
Petru Mot <pmot.external@epo.org>
2021-03-04 13:26:25 +0000

    added fees

--- Seminar/src/ro/ase/csie/cts/seminar2/Main.java
+++ Seminar/src/ro/ase/csie/cts/seminar2/Main.java
@@ -3,6 +3,8 @@ package ro.ase.csie.cts.seminar2;
 import java.math.BigDecimal;
 
 import ro.ase.csie.cts.seminar2.solid.BankAccount;
+import ro.ase.csie.cts.seminar2.solid.FeeBankAccount;
+import ro.ase.csie.cts.seminar2.solid.InsuficientFundsException;
 import ro.ase.csie.cts.seminar2.solid.Person;
 
 public class Main {
@@ -12,6 +14,32 @@ public class Main {
 		
 		Person p = new Person("Petru");
 		BankAccount account = new BankAccount("INGB123242342", p);
+		
+		account.deposit(100);
+		try {
+			account.withdraw(30);
+			account.withdraw(40);
+			account.withdraw(70);
+
+		} catch (InsuficientFundsException e) {
+			System.out.println(e.getMessage());
+		}
+			
+		System.out.println(account.getBalance());
+		
+		
+		account = new FeeBankAccount("INGB123123234234", p);
+		
+		account.deposit(500);
+		
+		try {
+			account.withdraw(100);
+		} catch (InsuficientFundsException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println(account.getBalance());
+		
 
 
 	}
--- Seminar/src/ro/ase/csie/cts/seminar2/solid/BankAccount.java
+++ Seminar/src/ro/ase/csie/cts/seminar2/solid/BankAccount.java
@@ -3,23 +3,26 @@ package ro.ase.csie.cts.seminar2.solid;
 public class BankAccount {
 	private String iban;
 	private long balance;
-	
+
 	private Person accountHolder;
-	
+
 	public BankAccount(String iban, Person person) {
 		this.iban = iban;
 		this.accountHolder = person;
 		balance = 0;
 	}
-	
-	public void withdraw(long amount) {
+
+	public void withdraw(long amount) throws InsuficientFundsException {
+		if (amount > balance)
+			throw new InsuficientFundsException("Insuficient funds " + balance);
+		System.out.println("withdrawing " + amount + " from " + iban);
 		balance -= amount;
 	}
-	
+
 	public void deposit(long amount) {
+		System.out.println("Adding " + amount + " to " + iban);
 		balance += amount;
 	}
-	
 
 	public String getIban() {
 		return iban;
@@ -32,6 +35,5 @@ public class BankAccount {
 	public Person getAccountHolder() {
 		return accountHolder;
 	}
-	
 
 }
